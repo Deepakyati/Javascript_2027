45f6bc97736da5a004ec413b791e6b1d20ac8123
Deepak18 <deepakyati01@gmail.com>
2026-08-29 22:03:14 +0530

    new file n

--- basics2.js
+++ basics2.js
@@ -1,3 +1,22 @@
-for(let i=0; i<10; i++){
-    console.log(i);
+/*
+let j=1
+for(let i=1; i<=20; i++){
+    if(i%2==0){
+        console.log("2 X "+ j +" = "+(2*j));
+        j++;
+    }
 }
+
+
+*/
+
+let k=0;
+for (let i=1; i<=100; i++){
+        if(i%2==0 && i%3==0){
+            k++;
+            console.log(i);
+            if (k==3){
+                break;
+            }
+        }
+    }
